c4f948240d085f8875eb2f64f54e12a0ca852f57
Crystal Broach <crysland@gmail.com>
2019-11-03 15:12:38 -0600

    getting the boards with appropriate user pin information in console log

--- src/javascripts/helpers/data/smash.js
+++ src/javascripts/helpers/data/smash.js
@@ -1,14 +1,29 @@
 import userData from './userData';
 import userBoards from './boardData';
+import userPins from './userPins';
 
 const getCompleteUserDatas = () => new Promise((resolve, reject) => {
-  userData.getUsers()
-    .then((singleUser) => userBoards.getBoardsByUid(singleUser.uid))
-    .then((board) => {
-      const boardsToDisplay = userBoards.getBoardsByUid(board[0].uid);
-      resolve(boardsToDisplay);
-    })
+  userData.getUsers().then((singleUser) => {
+    userBoards.getBoardsByUid(singleUser.uid).then((boards) => {
+      userPins.getUserPinsByUid(singleUser.uid).then((boardPins) => {
+        const finalBoards = [];
+        boards.forEach((board) => {
+          const newBoard = { ...board };
+          const userPinCollection = boardPins;
+          const filteredBoardPins = userPinCollection.filter((x) => x.boardId === board.id);
+          newBoard.pinItems = filteredBoardPins;
+          finalBoards.push(newBoard);
+        });
+        resolve(finalBoards);
+      });
+    });
+  })
     .catch((error) => reject(error));
 });
+
+// 1. get boards for user that is logged in
+// 2. get userPins for that board
+// 3. grab pins that match the pinId from user
+
 
 export default { getCompleteUserDatas };
